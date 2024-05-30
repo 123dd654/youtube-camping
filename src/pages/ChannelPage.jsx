5483208925a4e04cc48e3770c7778c0ba41197d7
123dd654 <161431124+123dd654@users.noreply.github.com>
2024-05-30 22:20:58 +0900

    2024.05.30

--- src/pages/ChannelPage.jsx
+++ src/pages/ChannelPage.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Main from '../components/section/Main'
+import { useParams } from 'react-router-dom';
+
+// import { CiBadgeDollar } from "react-icons/ci";
+// import { CiMedal } from "react-icons/ci";
+// import { CiRead } from "react-icons/ci";
 
 const ChannelPage = () => {
+    const { channelID } = useParams();
+    const [channelDetail, setChannelDetail] = useState(null);
+    const [channelVideo, setChannelVideo] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [nextPageToken, setNextPageToken] = useState(null);
+
+    useEffect(() => {
+        const fetchResults = async () => {
+            try {
+                const detail = await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet,brandingSettings,statistics&id=${channelID}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
+                const detaildata = await detail.json();
+                setChannelDetail(detaildata.items[0]);
+                console.log(detaildata);
+
+                const video = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelid=${channelID}$order=date&maxResults=48&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
+                const videoData = await video.json();
+                setChannelVideo(videoData.items);
+                setNextPageToken(videoData.nextPageToken);
+                // console.log(videoData);
+
+            } catch (error) {
+                console.log('데이터를 가져오지 못했습니다', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchResults();
+    }, [channelID]);
     return (
         <Main
             title="유튜버 채널"
-            description="유튜버 채널 페이지입니다.">
-            ChannelPage
+            description="유튜버 채널 페이지입니다."
+        >
+            <section id='channelPage'>
+                {channelDetail && (
+                    <div className='channel__inner'>
+                        <div className='channel__header' style={{ backgroundImage: `url(${channelDetail.brandingSettings.image.bannerExternalUrl})` }}>
+                            <div className='circle'>
+                                <img src={channelDetail.snippet.thumbnails.high.url} alt={channelDetail.snippet.title} />
+                            </div>
+                        </div>
+                        <div className="channel__info">
+                            <h3 className='title'>{channelDetail.snippet.title}</h3>
+                            <p className='desc'>{channelDetail.snippet.description}</p>
+                            <div className='info'>
+                                {/* <span><CiBadgeDollar />{channelDetail.statistics.subscriberCount}</span>
+                                <span><CiMedal />{channelDetail.statistics.videoCount}</span>
+                                <span><CiRead />{channelDetail.statistics.viewCount}</span> */}
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </section>
+
         </Main>
     )
 }
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Main from '../components/section/Main'
 import { Link, useParams } from 'react-router-dom'
 import Loading from '../components/section/Loading';
+import VideoView from '../components/video/VideoView';
 
 const SearchPage = () => {
     const { searchID } = useParams();
@@ -10,6 +11,7 @@ const SearchPage = () => {
 
     useEffect(() => {
         const fetchVideos = async () => {
+            setLoading(true);
             try {
                 const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&q=${searchID}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
                 const data = await response.json();
@@ -37,26 +39,9 @@ const SearchPage = () => {
                 <Loading />
             ) : (
                 <section id='searchPage' className='fade-in'>
-                    <h2>⇢ <em>{searchID}</em>를 검색한 결과입니다.</h2>
+                    <h2>🤪 <em>{searchID}</em>를 검색한 결과입니다.</h2>
                     <div className="video__inner">
-                        {videos.map((video, index) => (
-                            <div className='video' key={index} >
-                                <div className="video__thumb play__icon">
-                                    <Link
-                                        to={`/video/${video.id.videoId}`}
-                                        style={{ backgroundImage: `url(${video.snippet.thumbnails.high.url})` }}>
-                                    </Link>
-                                </div>
-                                <div className="video__info">
-                                    <div className='title'>
-                                        <Link to={`/video/${video.id.videoId}`}>{video.snippet.title}</Link>
-                                    </div>
-                                    <div className='author'>
-                                        <Link to={`/channel/${video.snippet.channelId}`}>{video.snippet.channelTitle}</Link>
-                                    </div>
-                                </div>
-                            </div >
-                        ))}
+                        <VideoView videos={videos} />
                     </div>
                 </section>
             )}
